3a13580d656d2e194afb288964821d1938d25055
padi-dev-vietnt2 <vietnt2@paditech.com>
2022-06-14 16:40:06 +0700

    example 6-9

--- contracts/Fundraiser.sol
+++ contracts/Fundraiser.sol
@@ -1,9 +1,31 @@
 pragma solidity >0.4.23 <0.9.0;
 
-contract Fundraiser {
-    string public name;
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-    constructor(string memory _name) public {
+contract Fundraiser is Ownable {
+    string public name;
+    string public url;
+    string public imageURL;
+    string public description;
+
+    address payable public beneficiary;
+    address public custodian;
+
+    constructor(
+        string memory _name,
+        string memory _url,
+        string memory _imageURL,
+        string memory _description,
+        address payable _beneficiary,
+        address _custodian
+    ) 
+        public 
+    {
         name = _name;
+        url = _url;
+        imageURL = _imageURL;
+        description = _description;
+        beneficiary = _beneficiary;
+        _transferOwnership(_custodian);
     } 
 }
--- test/fundraiser_test.js
+++ test/fundraiser_test.js
@@ -2,16 +2,55 @@ const FundraiserContract = artifacts.require("Fundraiser");
 
 contract("Fundraiser", accounts => {
     let fundraiser;
-    const name = "Beneficiary Name"
+    const name = "Beneficiary Name";
+    const url = "beneficiaryname.org";
+    const imageURL = "https://placekitten.com/600/350";
+    const description = "Beneficiary descripton";
+
+    const beneficiary = accounts[1];
+    const owner = accounts[0];
 
     describe("initialization", () => {
         beforeEach (async () => {
-            fundraiser = await FundraiserContract.new(name);
+            fundraiser = await FundraiserContract.new(
+                name,
+                url,
+                imageURL,
+                description,
+                beneficiary,
+                owner
+            );
         });
         
         it("gets the beneficiary name", async () => {
             const actual = await fundraiser.name();
             assert.equal(actual, name, "names should match");
         });
+
+        it("gets the beneficiary url", async () => {
+            const actual = await fundraiser.url();
+            assert.equal(actual, url, "url should match");
+        });
+
+        it("gets the beneficiary image url", async () => {
+            const actual = await fundraiser.imageURL();
+            assert.equal(actual, imageURL, "imageURL should match");
+        });
+
+        it("gets the beneficiary description", async () => {
+            const actual = await fundraiser.description();
+            assert.equal(actual, description, "description should match");
+        });
+
+        it("gets the beneficiary", async () => {
+            const actual = await fundraiser.beneficiary();
+            assert.equal(actual, beneficiary, "beneficiary address should match");
+        });
+
+        it("gets the owner", async () => {
+            const actual = await fundraiser.owner();
+            assert.equal(actual, owner, "bios should match");
+        });
     });
+
 });
